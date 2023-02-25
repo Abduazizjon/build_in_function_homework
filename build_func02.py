cd526ecdac553347faeb60f0125d12f8566b5f6d
Abduazizjon <nodirchik1988@gmail.com>
2023-02-25 11:33:11 +0500

    2done

--- build_func02.py
+++ build_func02.py
@@ -1,4 +1,4 @@
-def main():
+def main(javob):
     """Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func02 
 
@@ -8,4 +8,17 @@ def main():
     Returns:
         float: the value of the expression
     """
-    return
+    return javob
+
+a = 3
+b = 7
+v = 5
+g = 9
+d = 4
+
+
+javob = a*((b/v)-(g/d))
+
+print(javob)
+
+
